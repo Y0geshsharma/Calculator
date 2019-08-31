efd65a7810dc8fcb09bcb1f9d9b5be0d2837757e
shruti <bshantanu1@gmail.com>
2019-08-31 15:05:25 +0530

    branch added shantanu

--- calculator.java
+++ calculator.java
@@ -1,5 +1,9 @@
 import java.util.*;
+
 public class calculator{
+    private float divide(int a,int b){
+        return a/b;
+    }
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     System.out.println("----------Calculator---------");
@@ -7,14 +11,6 @@ public class calculator{
     System.out.println("<2> Subtract");
     System.out.println("<3> Multiply");
     System.out.println("<4> Divide");
-    System.out.print("Input: ");
-    int inp=sc.nextInt();
-    
-    switch(inp){
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-    }
+    System.out.print("Input: ");   
  }   
 }
