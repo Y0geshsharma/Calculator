4c111faccdfc1d8d7ebef3de1d2e66d7d25c772f
shruti <vtshruti@gmail.com>
2019-08-31 14:37:08 +0530

    initial commit

---
+++ calculator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class calculator{
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("----------Calculator---------");
+    System.out.println("<1> Add");
+    System.out.println("<2> Subtract");
+    System.out.println("<3> Multiply");
+    System.out.println("<4> Divide");
+    System.out.print("Input: ");
+    int inp=sc.nextInt();
+    
+    switch(inp){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+    }
+ }   
+}
